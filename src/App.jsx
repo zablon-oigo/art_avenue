08012eec083cae2688cdf0b2c7480d75e4dcc6dd
zablon <zablonoigo51@gmail.com>
2023-11-14 18:47:42 +0300

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import library1 from './assets/images/library1.jpg'
 import library2 from './assets/images/library2.jpg'
 import library3 from './assets/images/library3.jpg'
 import Footer from './components/Footer'
-
 function App() {
     
   const images={
@@ -29,7 +28,7 @@ function App() {
 
   return (
     <>
-    <Header img={hero}/>
+   <Header img={hero}/>
     <Main  img={images}/>
     <Story img={images}/>
     <Exhibition img={images}/>
